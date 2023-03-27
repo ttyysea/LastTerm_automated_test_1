41957c7eef1054201f9daf567545df2f2c884a9b
ttyysea <111670925+ttyysea@users.noreply.github.com>
2023-03-27 11:24:14 +0700

    Update test_function.py

--- test_function.py
+++ test_function.py
@@ -1,33 +1,39 @@
 from function import validate_number
 
 
-def test_people_1_van_0_car_0():
+def test_people_1_van_0_car_0_input_1():
     input = 1
     expected_result = (1, 0, 0)
     actual_result = validate_number(input)
     assert expected_result == actual_result
 
-def test_people_0_van_1_car_0():
+def test_people_0_van_1_car_0_input_11():
     input = 11
     expected_result = (0, 1, 0)
     actual_result = validate_number(input)
     assert expected_result == actual_result
 
-def test_people_0_van_0_car_1():
+def test_people_0_van_0_car_1_input_4():
     input = 4
     expected_result = (0, 0, 1)
     actual_result = validate_number(input)
     assert expected_result == actual_result
 
-def test_people_1_van_1_car_1():
+def test_people_1_van_1_car_1_input_16():
     input = 16
     expected_result = (1, 1, 1)
     actual_result = validate_number(input)
     assert expected_result == actual_result
 
-def test_people_0_van_0_car_0():
+def test_people_0_van_0_car_0_input_0():
     input = 0
     expected_result = (0, 0, 0)
+    actual_result = validate_number(input)
+    assert expected_result == actual_result
+
+def test_people_1_van_9_car_1_input_104():
+    input = 104
+    expected_result = (1, 9, 1)
     actual_result = validate_number(input)
     assert expected_result == actual_result
 
@@ -37,11 +43,14 @@ def test_number_not_integer_input_abc():
     actual_result = validate_number(input)
     assert expected_result == actual_result
 
+def test_number_not_integer_input_1dot1():
+    input = 1.1
+    expected_result = "number is not integer."
+    actual_result = validate_number(input)
+    assert expected_result == actual_result
+
 def test_number_less_than_0_input_minus_1():
     input = -1
     expected_result = "The number must be greater than 0."
     actual_result = validate_number(input)
     assert expected_result == actual_result
-
-
-
